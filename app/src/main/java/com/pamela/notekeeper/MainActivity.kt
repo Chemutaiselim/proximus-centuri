312a61721a48c1c8fec972f840138758e6d3fb1c
Selim <selimchemutai19@gmail.com>
2021-08-25 15:51:26 +0300

    Oncreate Method

--- app/src/main/java/com/pamela/notekeeper/MainActivity.kt
+++ app/src/main/java/com/pamela/notekeeper/MainActivity.kt
@@ -2,10 +2,20 @@ package com.pamela.notekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity)
+
+        /*
+        on the oncreate method is where we do most of the UI initializations
+         */
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
     }
 }
