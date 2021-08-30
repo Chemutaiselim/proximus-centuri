9674be93a7dcfc7ce520eeb1777c74a521e96883
Selim <selimchemutai19@gmail.com>
2021-08-30 12:40:03 +0300

    Commit to Master

--- app/src/main/java/com/pamela/notekeeper/MainActivity.kt
+++ app/src/main/java/com/pamela/notekeeper/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
 
         /*
